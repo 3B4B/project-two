24cf80ddc014bf535293dbb5269355fa7abb24a5
Jimmy Kane <jbk5603@psu.edu>
2021-10-12 14:32:19 -0400

    Base HTML in Copied Card

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 import { LearningCard } from './LearningCard.js';
+import { LearningCardCopy } from './LearningCardCopy.js';
 
 customElements.define(LearningCard.tag, LearningCard);
+customElements.define(LearningCardCopy.tag, LearningCardCopy);
