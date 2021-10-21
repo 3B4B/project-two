d63ab9b8805de0ffce06d2b024a819fd8d993439
Jimmy Kane <jbk5603@psu.edu>
2021-10-21 14:50:31 -0400

    Renamed Card and added multiple implementations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
 import { LearningCard } from './LearningCard.js';
-import { LearningCardCopy } from './LearningCardCopy.js';
 
 customElements.define(LearningCard.tag, LearningCard);
-customElements.define(LearningCardCopy.tag, LearningCardCopy);
