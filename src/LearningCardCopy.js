34be3b5e9189ff62b0f9bac996dd03ab0a840a96
Francesca Ansell <fla5029@psu.edu>
2021-10-21 14:40:39 -0400

    updates

--- src/LearningCardCopy.js
+++ src/LearningCardCopy.js
@@ -72,7 +72,6 @@ export class LearningCardCopy extends LitElement {
         background-color: transparent;
       } */
       #entire-card {
-        border-width: 20px solid black;
         max-width: 700px;
         font-family: 'Open Sans', sans-serif;
       }
@@ -83,9 +82,7 @@ export class LearningCardCopy extends LitElement {
   render() {
     return html`
       <div id="entire-card">
-        <div id="scaffold-card">
-          <learning-scaffold type=${this.type}></learning-scaffold>
-        </div>
+        <learning-scaffold type=${this.type}></learning-scaffold>
       </div>
     `;
   }
