4692988f5eaf5a79f6b2fe9201a74d13a5fcb9ef
Jimmy Kane <jbk5603@psu.edu>
2021-10-28 14:40:41 -0400

    Finished tests

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,9 @@ import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
 
 import '../src/app.js';
+import '../src/LearningScaffold.js';
+import '../src/LearningIcon.js';
+import '../src/LearningBanner.js';
 
 describe('LearningCard', () => {
   let element;
@@ -16,7 +19,13 @@ describe('LearningCard', () => {
   it('renders an learning-banner', () => {
     const banner = element.shadowRoot.querySelector('learning-banner');
     expect(banner).to.exist;
-    // expect(parag.textContent).to.equal('I am content');
+  });
+
+  it('renders content', () => {
+    const paragraph = element.shadowRoot.querySelector('slot[name="content"]');
+    expect(paragraph.assignedElements({ flat: true })[0].innerText).to.equal(
+      'I am content'
+    );
   });
 
   it('passes the a11y audit', async () => {
@@ -28,47 +37,23 @@ describe('Learning-Scaffold', () => {
   let element;
   beforeEach(async () => {
     element = await fixture(
-      html`<learning-scaffold type="CHEM CONNECTION"
-        ><learning-banner
-          type="CHEM CONNECTION"
-          slot="banner"
-        ></learning-banner>
+      html`<learning-scaffold type="chem"
+        ><learning-banner type="chem" slot="banner"></learning-banner>
         <p slot="content">What am I thinking</p></learning-scaffold
       >`
     );
   });
 
   it('renders a paragraph', () => {
-    const parag = element.shadowRoot.querySelector('p');
-    expect(parag).to.exist;
-  });
-
-  it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible();
-  });
-});
-
-describe('Learning-Banner', () => {
-  let element;
-  beforeEach(async () => {
-    element = await fixture(
-      html`<learning-banner type="LEARNING OBJECTIVES" slot="banner">
-        <learning-icon type="LEARNING OBJECTIVES" slot="icon"></learning-icon>
-        <h2 id="main-header" slot="heading">Unit 1</h2>
-        <h3 id="sub-header" slot="subHeading">"LEARNING OBJECTIVES"</h3>
-      </learning-banner>`
+    const parag = element.shadowRoot.querySelector('slot[name="content"]');
+    expect(parag.assignedElements({ flat: true })[0].innerText).to.equal(
+      'What am I thinking'
     );
   });
 
-  // Fails, how come this does not find the icon, it's passed in as a slot?
-  it('renders a learning-icon', () => {
-    const icon = element.shadowRoot.querySelector('slot[name="icon"]'); // slot
-    expect(icon.assignedElements({ flat: true })[0].innerText).to.exist;
-  });
-
-  it('renders a main heading', () => {
-    const icon = element.shadowRoot.querySelector('slot[name="heading"]');
-    expect(icon.assignedElements({ flat: true })[0].innerText).to.exist;
+  it('renders an learning-banner', () => {
+    const banner = element.shadowRoot.querySelector('slot[name="banner"]');
+    expect(banner).to.exist;
   });
 
   it('passes the a11y audit', async () => {
@@ -85,8 +70,44 @@ describe('Learning-Icon', () => {
   });
 
   it('renders a paragraph', () => {
-    const parag = element.shadowRoot.querySelector('img');
-    expect(parag).to.exist;
+    const img = element.shadowRoot.querySelector('img');
+    expect(img).to.exist;
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Learning-Banner', () => {
+  let element;
+  beforeEach(async () => {
+    element = await fixture(
+      html`<learning-banner type="LEARNING OBJECTIVES">
+        <learning-icon type="LEARNING OBJECTIVES" slot="icon"></learning-icon>
+        <h2 id="main-header" slot="heading">Unit 1</h2>
+        <h3 id="sub-header" slot="subHeading">LEARNING OBJECTIVES</h3>
+      </learning-banner>`
+    );
+  });
+
+  // Fails, how come this does not find the icon, it's passed in as a slot?
+  it('renders a learning-icon', () => {
+    const icon = element.shadowRoot.querySelector(`slot[name="icon"]`);
+    expect(icon.assignedElements({ flat: true })[0]).to.exist;
+  });
+
+  it('renders a main heading', () => {
+    const heading = element.shadowRoot.querySelector(`slot[name="heading"]`);
+    expect(heading.assignedElements({ flat: true })[0].innerText).to.equal(
+      'Unit 1'
+    );
+  });
+  it('renders a sub heading', () => {
+    const heading = element.shadowRoot.querySelector(`slot[name="subHeading"]`);
+    expect(heading.assignedElements({ flat: true })[0].innerText).to.equal(
+      'LEARNING OBJECTIVES'
+    );
   });
 
   it('passes the a11y audit', async () => {
