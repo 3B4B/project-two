ca241e0ea47b72c3ccb2f37c412621b831fd487b
Francesca Ansell <fla5029@psu.edu>
2021-10-28 14:25:01 -0400

    fixing types and button

--- src/LearningCard.js
+++ src/LearningCard.js
@@ -1,62 +1,53 @@
 /* eslint-disable no-unused-vars */
-// dependencies / things imported
 import { LitElement, html, css } from 'lit';
 import './LearningScaffold.js';
-import '@3b4b/circle-button-fj/circle-button-fj';
 
-// EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
-// which has the magic life-cycles and developer experience below added
 export class LearningCard extends LitElement {
-  // a convention I enjoy so you can change the tag name in 1 place
   static get tag() {
     return 'learning-card';
   }
 
-  // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
     this.myIcon = null;
-    this.type = 'LEARNING OBJECTIVES';
-
-    // setTimeout(() => {
-    //   import('./LearningBanner.js');
-    //   import('./LearningIcon.js');
-    // }, 0);
+    this.type = 'know';
+    this.subheader = 'DID YOU KNOW?';
   }
 
-  // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
   static get properties() {
     return {
       type: { type: String, reflect: true },
       link: { type: String, reflect: true },
+      subheader: { type: String, reflect: true },
     };
   }
 
-  // updated fires every time a property defined above changes
-  // this allows you to react to variables changing and use javascript to perform logic
-  updated(changedProperties) {}
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === 'type' && this[propName] === 'chem') {
+        this.subheader = 'CHEM CONNECTION';
+      } else if (propName === 'type' && this[propName] === 'know') {
+        this.subheader = 'DID YOU KNOW?';
+      } else if (propName === 'type' && this[propName] === 'objectives') {
+        this.subheader = 'LEARNING OBJECTIVES';
+      }
+    });
+  }
 
-  // Lit life-cycle; this fires the 1st time the element is rendered on the screen
-  // this is a sign it is safe to make calls to this.shadowRoot
   firstUpdated(changedProperties) {
     if (super.firstUpdated) {
       super.firstUpdated(changedProperties);
     }
   }
 
-  // HTMLElement life-cycle, element has been connected to the page / added or moved
-  // this fires EVERY time the element is moved
   connectedCallback() {
     super.connectedCallback();
   }
 
-  // HTMLElement life-cycle, element has been removed from the page OR moved
-  // this fires every time the element moves
   disconnectedCallback() {
     super.disconnectedCallback();
   }
 
-  // CSS - specific to Lit
   static get styles() {
     return css`
       :host {
@@ -82,9 +73,6 @@ export class LearningCard extends LitElement {
         /* border: 1px solid yellow; */
         margin: 0;
       }
-      circle-button-fj {
-        margin-left: 80%;
-      }
     `;
   }
 
@@ -96,16 +84,10 @@ export class LearningCard extends LitElement {
           <learning-banner type=${this.type} slot="banner">
             <learning-icon type=${this.type} slot="icon"></learning-icon>
             <h2 class="main-header" slot="heading">UNIT 1</h2>
-            <h3 class="sub-header" slot="subHeading">${this.type}</h3>
+            <h3 class="sub-header" slot="subHeading">${this.subheader}</h3>
           </learning-banner>
-
           <div slot="content">
             <slot name="content"></slot>
-            <circle-button-fj
-              title="Help"
-              description="Click here"
-              link="https://quizlet.com/latest"
-            ></circle-button-fj>
           </div>
         </learning-scaffold>
       </div>
