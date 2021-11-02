051c3e7680141bac8fca0c47050a41b775e0d825
Francesca <fla5029@psu.edu>
2021-11-02 14:27:27 -0400

    added lazy loading

--- src/LearningCard.js
+++ src/LearningCard.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { LitElement, html, css } from 'lit';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { IntersectionObserverMixin } from '@lrnwebcomponents/intersection-element/lib/IntersectionObserverMixin.js';
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
 
-export class LearningCard extends LitElement {
+export class LearningCard extends IntersectionObserverMixin(SimpleColors) {
   static get tag() {
     return 'learning-card';
   }
@@ -19,6 +22,10 @@ export class LearningCard extends LitElement {
   }
 
   static get properties() {
+    let props = {};
+    if (super.properties) {
+      props = super.properties;
+    }
     return {
       type: { type: String, reflect: true },
       link: { type: String, reflect: true },
@@ -54,134 +61,142 @@ export class LearningCard extends LitElement {
   }
 
   static get styles() {
-    return css`
-      :host {
-        font-family: 'Open Sans', sans-serif;
-        margin: 50px;
-      }
-      .entire-card {
-        font-family: 'Open Sans', sans-serif;
-        width: 900px;
-        transition: all 0.35s ease-in-out;
-      }
+    return [
+      ...super.styles,
 
-      .main-header {
-        font-weight: 300;
-        font-size: 50px;
-        /* border: 1px solid yellow; */
-        margin: 0;
-      }
-
-      .sub-header {
-        font-weight: 500;
-        font-size: 50px;
-        /* border: 1px solid yellow; */
-        margin: 0;
-      }
-
-      .content {
-        background-color: white;
-        border: 1px solid black;
-        border-top: transparent;
-        padding-top: 5px;
-        padding-bottom: 10px;
-        padding-left: 150px;
-        margin: 0px;
-      }
-
-      .content ::slotted(circle-button-fj) {
-        margin-left: 260px;
-      }
-
-      /* max-width: "If device width is less than or equal to 600px, then do {…}” 
-        min-width: “If [device width] is greater than or equal to 600px, then do {…}”
-        Combo: min-width is bottom value, max-width is top for */
-      @media screen and (min-width: 300px) and (max-width: 650px) {
+      css`
+        :host {
+          font-family: 'Open Sans', sans-serif;
+          margin: 50px;
+        }
         .entire-card {
+          font-family: 'Open Sans', sans-serif;
+          width: 900px;
           transition: all 0.35s ease-in-out;
-          width: 400px;
         }
 
         .main-header {
           font-weight: 300;
-          font-size: 40px;
+          font-size: 50px;
           /* border: 1px solid yellow; */
           margin: 0;
         }
 
         .sub-header {
           font-weight: 500;
-          font-size: 40px;
+          font-size: 50px;
           /* border: 1px solid yellow; */
           margin: 0;
         }
 
         .content {
-          padding-top: 5px;
-          padding-left: 10px;
-          padding-bottom: 20px;
-          padding-right: 30px;
-          margin: 0px;
-        }
-
-        /* TODO: Target circle-button-fj's css to fix margin */
-        .content ::slotted(circle-button-fj) {
-          margin-left: 148px;
-        }
-      }
-
-      @media screen and (min-width: 650px) and (max-width: 1000px) {
-        .entire-card {
-          transition: all 0.35s ease-in-out;
-          width: 600px;
-        }
-
-        .main-header {
-          font-weight: 300;
-          font-size: 45px;
-          /* border: 1px solid yellow; */
-          margin: 0;
-        }
-
-        .sub-header {
-          font-weight: 500;
-          font-size: 45px;
-          /* border: 1px solid yellow; */
-          margin: 0;
-        }
-
-        .content {
+          background-color: white;
+          border: 1px solid black;
+          border-top: transparent;
           padding-top: 5px;
           padding-bottom: 10px;
-          padding-left: 50px;
+          padding-left: 150px;
           margin: 0px;
         }
 
-        /* TODO: Target circle-button-fj's css to fix margin */
         .content ::slotted(circle-button-fj) {
-          margin-left: 196px;
+          margin-left: 260px;
         }
-      }
-    `;
+
+        /* max-width: "If device width is less than or equal to 600px, then do {…}” 
+        min-width: “If [device width] is greater than or equal to 600px, then do {…}”
+        Combo: min-width is bottom value, max-width is top for */
+        @media screen and (min-width: 300px) and (max-width: 650px) {
+          .entire-card {
+            transition: all 0.35s ease-in-out;
+            width: 400px;
+          }
+
+          .main-header {
+            font-weight: 300;
+            font-size: 40px;
+            /* border: 1px solid yellow; */
+            margin: 0;
+          }
+
+          .sub-header {
+            font-weight: 500;
+            font-size: 40px;
+            /* border: 1px solid yellow; */
+            margin: 0;
+          }
+
+          .content {
+            padding-top: 5px;
+            padding-left: 10px;
+            padding-bottom: 20px;
+            padding-right: 30px;
+            margin: 0px;
+          }
+
+          /* TODO: Target circle-button-fj's css to fix margin */
+          .content ::slotted(circle-button-fj) {
+            margin-left: 148px;
+          }
+        }
+
+        @media screen and (min-width: 650px) and (max-width: 1000px) {
+          .entire-card {
+            transition: all 0.35s ease-in-out;
+            width: 600px;
+          }
+
+          .main-header {
+            font-weight: 300;
+            font-size: 45px;
+            /* border: 1px solid yellow; */
+            margin: 0;
+          }
+
+          .sub-header {
+            font-weight: 500;
+            font-size: 45px;
+            /* border: 1px solid yellow; */
+            margin: 0;
+          }
+
+          .content {
+            padding-top: 5px;
+            padding-bottom: 10px;
+            padding-left: 50px;
+            margin: 0px;
+          }
+
+          /* TODO: Target circle-button-fj's css to fix margin */
+          .content ::slotted(circle-button-fj) {
+            margin-left: 196px;
+          }
+        }
+      `,
+    ];
   }
 
   render() {
     // Entire Card
     return html`
-      <div class="entire-card">
-        <learning-scaffold type=${this.type}>
-          <div slot="banner">
-            <learning-banner type=${this.type} slot="banner">
-              <learning-icon type=${this.type} slot="icon"></learning-icon>
-              <h2 class="main-header" slot="heading">${this.header}</h2>
-              <h3 class="sub-header" slot="subHeading">${this.subheader}</h3>
-            </learning-banner>
-          </div>
-          <div slot="content" class="content">
-            <slot name="content"></slot>
-          </div>
-        </learning-scaffold>
-      </div>
+      ${this.elementVisible
+        ? html` <div class="entire-card">
+            <learning-scaffold type=${this.type}>
+              <div slot="banner">
+                <learning-banner type=${this.type} slot="banner">
+                  <learning-icon type=${this.type} slot="icon"></learning-icon>
+                  <h2 class="main-header" slot="heading">${this.header}</h2>
+                  <h3 class="sub-header" slot="subHeading">
+                    ${this.subheader}
+                  </h3>
+                </learning-banner>
+              </div>
+              <div slot="content" class="content">
+                <slot name="content"></slot>
+              </div>
+            </learning-scaffold>
+          </div>`
+        : ``}
     `;
   }
 
